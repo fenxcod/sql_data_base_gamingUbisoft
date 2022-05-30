bd6fa8676dc319f87915f09f491093cd8a1f58dc
fenxcod <alanrgfenxcod@gmail.com>
2022-05-30 17:36:43 -0500

    Agregando inserciones

---
+++ insert_into_gaming_ubisoft.sql
@@ -0,0 +1,81 @@
+/*
+ESTA INSERCIÓN SE DIO CON EL USR PROFESORA_LILI
+*/
+insert into add_users(`name_and_surname`,`correo`,`contrasena`,`fecha_nacimiento`)
+values ('Alan Rosas Garcia','alan_rg@tesch.edu.mx','esto es administracion db','1997-07-18 18:05:00'),
+('Paulo Petry ','paulo.petry@relal.org.co','fvdfvsd vsdccsdcsc','1995-04-18 18:05:00'),
+('Víctor Manuel Ramos Carrión','vimanur@hotmail.com','sdk sdsdxcdscsdks','1997-07-18 18:05:00'),
+('Juan Carlos Valadez Martínez ','jcarlosvaladez@hotmail.com',' kdckwdcwelqldqmwd','1997-07-18 18:05:00'),
+('Sergio Alejandro Franco ','comisiondevocaciones@lasalle.org.ar','dwfwefoewwede','1997-07-18 18:05:00'),
+('Nicolás Alejandro Falcone','nfalcone@delasalle.com.ar','dvjnwdvc asc','1997-07-18 18:05:00'),
+('Fernando Fontanellaz','ffontanellaz@lasalle.org.ar','dsfwefweffqq','1997-07-18 18:05:00'),
+(' Valentina Morande','valentinamorande@hotmail.com','fkwd fwefeew','1997-07-18 18:05:00'),
+('Cesar Andrés Carvajal C.','caccfsc@yahoo.com','caccfsc@yahoo.com','1997-07-18 18:05:00'),
+(' Sebastián Cornejo Torres','scornejot@hotmail.com','scornejot@hotmail.com','1997-07-18 18:05:00'),
+('Gerardo Quinteros Nogales','salleor@hotmail.com','salleor@hotmail.com','1997-07-18 18:05:00'),
+('Eder Polido','eder.polido@lasalle.org.br','eder.polido@lasalle.org.br','1997-07-18 18:05:00'),
+('Rodrigo Da Silva','rodrigo.silva@lasalle.org.br','rodrigo.silva@lasalle.org.br','1997-07-18 18:05:00'),
+('Junior Schnorrenberger','junior.schnorrenberger@lasalle.org.br','junior.schnorrenberger@lasalle.org.br','1997-07-18 18:05:00'),
+('Dan Cruz Mora','hnopastoralca@lasalleca.org','hnopastoralca@lasalleca.org','1997-07-18 18:05:00'),
+('Juan Carlos Santos Pivaral ','juanpivaral@hotmail.com','juanpivaral@hotmail.com','1997-07-18 18:05:00'),
+('Maria José de la Roca González','mjdelaroca@lasalleca.org','mjdelaroca@lasalleca.org','1997-07-18 18:05:00'),
+('David Garcia de la Peña','davidfsc@gmail.com','davidfsc@gmail.com','1997-07-18 18:05:00'),
+('Alexander Zapata','alexander.zapata@delasalle.edu.co','alexander.zapata@delasalle.edu.co','1997-07-18 18:05:00'),
+('José Eliecer Muñoz','jose.munoz@delasalle.edu.co','jose.munoz@delasalle.edu.co','1997-07-18 18:05:00'),
+('Luis Felix Romero ','luisfelixrh@yahoo.com','luisfelixrh@yahoo.com','1997-07-18 18:05:00'),
+('Rosario Yepez Garrido','hazanheymann@yahoo.com','hazanheymann@yahoo.com','1997-07-18 18:05:00'),
+('Nancy Revelo','saenzreveloandrea@hotmail.com','saenzreveloandrea@hotmail.com','1997-07-18 18:05:00'),
+('Narcisa Manzano','taty_20mvilca@hotmail.com','taty_20mvilca@hotmail.com','1997-07-18 18:05:00'),
+('Ximena Bautista ','luistroya1@hotmail.com','luistroya1@hotmail.com','1997-07-18 18:05:00'),
+('Luis Quispe Ponce ','hectormarceloq@hotmail.com','hectormarceloq@hotmail.com','1997-07-18 18:05:00'),
+('osé Luis Macías ','ht.henry@hotmail.com','ht.henry@hotmail.com','1997-07-18 18:05:00'),
+('Luis Maigua ','dmatak@yahoo.es','dmatak@yahoo.es','1997-07-18 18:05:00'),
+('Nelson Ruiz','dannyruiz89@yahoo.es','dannyruiz89@yahoo.es','1997-07-18 18:05:00'),
+('Estalin Sangoquiza ','anhrom@gmail.com','anhrom@gmail.com','1997-07-18 18:05:00'),
+('Andrés Talabela','andytalabela@hotmail.com','andytalabela@hotmail.com','1997-07-18 18:05:00'),
+('Elida Perez','badbyron@hotmail.com','badbyron@hotmail.com','1997-07-18 18:05:00'),
+('Verónica Muquiz ','carlosbanda86@hotmail.com','carlosbanda86@hotmail.com','1997-07-18 18:05:00'),
+('Génesis Morocho','dario_gabriel1990@hotmail.com','dario_gabriel1990@hotmail.com','1997-07-18 18:05:00'),
+('Luis Guerrero ','gximenazoo@yahoo.com','gximenazoo@yahoo.com','1997-07-18 18:05:00'),
+('Javier Zambrano ','francislean1983@hotmail.com','francislean1983@hotmail.com','1997-07-18 18:05:00'),
+('Marcelo Yánez Espinoza ','marceloyanezs@yahoo.es','marceloyanezs@yahoo.es','1997-07-18 18:05:00'),
+('Carlos Cárdenas','carlossantiago5_@hotmail.com','carlossantiago5_@hotmail.com','1997-07-18 18:05:00'),
+('Fabian Zamora','edu-ldu@hotmail.com','edu-ldu@hotmail.com','1997-07-18 18:05:00'),
+('Alicia Sandoval ','ali_angysamy@hotmail.es','ali_angysamy@hotmail.es','1997-07-18 18:05:00'),
+('Carmen Luz Reyes','camelupa@aol.com','camelupa@aol.com','1997-07-18 18:05:00'),
+('Blanca Angulo ','santyq_82@hotmail.com','santyq_82@hotmail.com','1997-07-18 18:05:00'),
+('Miriam Silva ','miryam.silva@yahoo.com','miryam.silva@yahoo.com','1997-07-18 18:05:00'),
+('Marieta Sanchez','ansimpresion@hotmail.com','ansimpresion@hotmail.com','1997-07-18 18:05:00'),
+('Pablo Orellana','pablis1956@hotmail.com','pablis1956@hotmail.com','1997-07-18 18:05:00');
+
+/*
+ESTA INSERCIÓN LA HIZO EL USR PROFESORA LILI, BOS UN CRACK
+*/
+insert into productos(`nombre`,`descripcion`,`fecha_registro`)
+values ('far cry 6','Far Cry 6 es un videojuego de acción first person shooter a cargo de Ubisoft Toronto y Ubisoft para PC, PlayStation 4, Xbox One, PlayStation 5, Xbox Series y Stadia','2022-05-30 13:12:00'),
+('rocksmith+','Aprende tocando junto a una enorme cantidad de tus canciones favoritas con retroalimentación en tiempo real, ritmo personalizable y una biblioteca de grabaciones oficiales cada vez más grande, todas diseñadas para ayudarte a expresar tu creatividad.','2022-05-30 13:26:00'),
+('riders republic','¡Únete en vivo más de 50 JUGADORES SIMULTÁNEAMENTE en pantalla y sé testigo de las mayores victorias y los peores fracasos de los demás en un mundo vivo y vibrante! Únete a los vibrantes centros sociales y conoce a la alocada comunidad de conductores. ¡Únete!','2022-05-30 13:30:00'),
+('Rainbow Six Extraction','Apareció una nueva amenaza. Enfréntate a nuevos enemigos, consigue tecnología REACT totalmente nueva, recompensas únicas y mucho más.','2022-05-30 13:12:00'),
+('Rainbow Six Mobile','Los operadores de élite de Rainbow Six unieron fuerzas para crear REACT (equipo de análisis y contención de exógenos de Rainbow), una organización altamente especializada.','2022-05-30 13:30:00'),
+('Just Dance 2022','¡Just Dance® 2022, el mejor juego de baile de todos, está de vuelta con 40 éxitos nuevos y modos de juego con los grandes éxitos del momento y los clásicos de siempre! ¿Buscas el juego perfecto para compartir con tu familia y amigos? ¡Just Dance 2022 es para ti!','2022-05-30 13:32:00'),
+('Mario + Rabbids Sparks of Hope','La fuerza maligna de Cursa corre desenfrenada por todo el universo. Quiere consumir a las misteriosas Sparks, unas extrañas y poderosas criaturas formadas por la fusión de Lumas y Rabbids, y amenaza con destruir a todos los que se interpongan en su camino','2022-05-30 13:33:00'),
+('Rainbow Six Siege','Distrae la atención del enemigo y lleva ventaja con Sens, la nueva incorporación al equipo Rainbow. Sens trae el nuevo fusil de asalto POF-9 y la habilidad Sistema de proyección ROUE, que permite bloquear líneas de visión y cambiar el curso de la batalla. Operation Vector Glare también presenta la Galería de tiro, además del nuevo mapa para Combate a muerte por equipos ambientado en Grecia. Con todo esto, dispondrás de nuevas formas para pulir tus habilidades y ponerlas a prueba.','2022-05-30 13:34:00'),
+('Assassins Creed Valhalla 1','Assassins Creed® Valhalla Dawn of Ragnarök, la expansión más ambiciosa en la historia de la franquicia. Disponible en la primavera de 2022','2022-05-30 13:35:00'),
+('Avatar: Frontiers of Pandora','Avatar: Frontiers of Pandora™ es un juego de acción y aventura en primera persona desarrollado por Massive Entertainment, un estudio de Ubisoft, en colaboración con Lightstorm Entertainment y Disney.','2022-05-30 13:36:00'),
+('Skull and Bones','Regístrate para tener la oportunidad de participar en pruebas futuras y fases en vivo, y enterarte de todas las novedades de Skull & Bones. También puedes participar del Programa Insider y ayudar a crear nuestra comunidad. Pronto llegarán más novedades.','2022-05-30 13:40:00'),
+('HUNGRY SHARK WORLD','¡La serie ganadora de premios Hungry Shark llega a consola! ¡Controla a 20 tiburones diferentes, encara batallas contra jefes y desbloquea misiones y tesoros escondidos!','2022-05-30 13:40:00'),
+('Una eterna arena de batalla','Brawlhalla es un juego de plataforma de luchas que puedes jugar totalmente gratis, donde los más grandes guerreros de la historia se enfrentan para demostrar quién es o será el más grande de todos. Ahora con Cross-Play para PS4, Xbox One, PC, Nintendo Switch, iOS y Android. Registre su dispositivo móvil','2022-05-30 13:41:00'),
+('Assassins Creed Valhalla','En Assassins Creed Valhalla eres Eivor, una feroz leyenda vikinga. Explora un bello y misterioso mundo abierto ambientado en el brutal trasfondo de la Inglaterra medieval. Saquea a tus enemigos, haz prosperar tu asentamiento y consolida tu poder político para hallar tu lugar entre los dioses en el Valhalla. ','2022-05-30 13:46:00'),
+('Watch Dogs Legion 1','Crea una resistencia a partir de prácticamente cualquier persona que veas conforme hackeas, te infiltras y luchas por recuperar del colapso a un Londres de un futuro cercano. Bienvenido a la res..','2022-05-30 13:48:00'),
+('Watch Dogs Legion ','Ultimate Edition - PC (descargar) ','2022-05-30 13:48:00'),
+('Ghost Recon Breakpoint','Conviértete en un soldado de élite de operaciones especiales conocido como Ghost, con una misión especial en Auroa. El remoto archipiélago en algún lugar del Pacífico Sur ha perdido contacto con el mundo exterior debido a que Skell Technology se ha vuelto hostil. Los Wolves, un equipo rebelde de operaciones especiales liderado por tu antiguo compañero de armas, se han apoderado de todo y te están cazando. Dirige a tu equipo a través de las vistas de la isla y salva a los civiles sin que nadie se de cuenta. ','2022-05-30 13:49:00');
+
+/*
+ESTA INSERCIÓN LA REALIZO EL USR CAPO, MASTER CHIF ALAN
+*/
+insert into categorias(`categoria`,`descripcion`)
+values ('Clasificacion A','Contenido para todo el publico.'),
+('Clasificacion B:','Contenido para adolescente a partir de 12'),
+('Clasificacion B15','Contenido para mayores de 15'),
+('Clasificacion C','Contenido no apto para menores de 18'),
+('Clasificacion D','Contenido extremo y exclusivo para adultos');
